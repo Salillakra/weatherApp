90d98bcbc7f369655e02836d73e7f7c9ff922af2
Salillakra <salillakra223@gmail.com>
2023-09-01 22:10:46 +0530

    added loading animation and hero section

---
+++ src/Loading.svelte
@@ -0,0 +1,9 @@
+<!-- overlay -->
+<div class="fixed inset-0 bg-[rgba(255,255,255,0.6)]" aria-hidden="true">
+  <img
+    src="../public/LoadingGIF.gif"
+    alt="Loading animation"
+    aria-label="Loading overlay"
+    class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-96 w-96"
+  />
+</div>
